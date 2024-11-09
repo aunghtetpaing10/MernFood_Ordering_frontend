4587347b9676199c41c0c2bd58fe1f8fe06f0476
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-11-09 17:32:49 +0700

    modified toast

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+
 type Props = {
-    placeholder: string;
-    onSubmit: (value: string) => void;
-}
+  placeholder: string;
+  onSubmit: (value: string) => void;
+};
 
-const SearchBar = () => {
+const SearchBar = ({ placeholder, onSubmit }: Props) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(inputValue);
+  };
+
   return (
-    <div>SearchBar</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
