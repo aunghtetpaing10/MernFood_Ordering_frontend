ea89518221bf2646ef0a6dc37fc6f996eb3e1078
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-11-19 00:07:24 +0700

    add update order status feature

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,12 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import OrderItemCard from "@/components/OrderItemCard";
 
 const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
@@ -11,15 +14,32 @@ const ManageRestaurantPage = () => {
     useCreateMyRestaurant();
   const { updateRestaurant, isPending: isUpdateLoading } =
     useUpdateMyRestaurant();
+  const { orders } = useGetMyRestaurantOrders();
 
   const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent value="orders" className="space-y-5 bg-gray-50 p-10 rounded-lg">
+        <h2 className="text-2xl font-bold">
+          {orders?.length ? `${orders.length} Orders` : "No orders yet"}
+        </h2>
+          {orders?.map((order) => (
+            <OrderItemCard key={order._id} order={order} />
+          ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
